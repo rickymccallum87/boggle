c580c0e246370abc5845b4bbdb75191f4b68c076
Ricky McCallum <rickymccallum87@gmail.com>
2017-12-02 21:15:41 -0500

    Simplify dimensions variable

--- boggle.py
+++ boggle.py
@@ -4,14 +4,14 @@
 board = []
 
 # Get board dimensions
-boardX = boardY = int(input('Width and height? '))
+dimensions = int(input('Width and height? '))
 
 # Generate a letter
 def generateLetter:
 	return 'a'
 
 # Display grid
-for i in range(boardY):
-	for j in range(boardX):
+for i in range(dimensions):
+	for j in range(dimensions):
 		print(generateLetter(), end=' ')	
 	print('')
