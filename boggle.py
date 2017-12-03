1c0fc6ff6aed69814aa7d967494d4b7679b7fa44
Ricky McCallum <rickymccallum87@gmail.com>
2017-12-02 21:23:35 -0500

    Define alphabet

--- boggle.py
+++ boggle.py
@@ -3,6 +3,9 @@
 # Initialize new board
 board = []
 
+# Define alphabet
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Qu', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
 # Get board dimensions
 dimensions = int(input('Width and height? '))
 
