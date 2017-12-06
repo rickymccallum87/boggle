30b14f54e2827e9b11692892cd9baa5a99675c52
Ricky McCallum <rickymccallum87@gmail.com>
2017-12-06 16:20:01 +0000

    Catch errors converting input to int

--- boggle.py
+++ boggle.py
@@ -6,7 +6,12 @@ from random import randint
 alphabet = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Qu', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
 
 # Get board dimensions
-dimensions = int(input('Width and height? '))
+dimensions = ''
+while dimensions == '':
+	try:
+		dimensions = int(input('Width and height? '))
+	except:
+		print('Please answer with an integer.')
 
 # Generate a letter
 def generateLetter():
